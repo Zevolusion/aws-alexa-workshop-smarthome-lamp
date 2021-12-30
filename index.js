2a9f386ef345e183f14e82731d698d066228b389
Zevolusion <zhangwraith@gmail.com>
2021-12-30 14:13:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,8 @@ const AWSIoT = require('aws-iot-device-sdk');
 const QRCode = require('qrcode');
 
 // Change the configuration below
-const thingName = 'ratchet';
+const thingName = 'smart_lamp_1';
 const iotEndpoint = 'abty4kifln98q-ats.iot.us-east-1.amazonaws.com';
-const deviceBindingUrl = 'https://xxxx.com/';
 
 const shadow = AWSIoT.thingShadow({
   keyPath: 'credentials/private.key',
@@ -35,11 +34,6 @@ shadow.on('connect', function () {
 
     console.info('connected to IoT Core...\n');
 
-    console.info('This is the QR Code shipped with the Device:');
-    QRCode.toString(`${deviceBindingUrl}?thingName=${thingName}`, {type: 'terminal'}, function (err, string) {
-      if (err) throw err;
-      console.log(string)
-    })
   })
 
 });
